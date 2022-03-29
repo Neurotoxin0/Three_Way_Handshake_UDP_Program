32023fe2149a7cc4c6681aeca7775b09164b6a3a
Don <Xusi101066@163.com>
2022-03-29 19:58:00 -0400

    Fix

--- Client.java
+++ Client.java
@@ -1,24 +1,45 @@
+/**
+ Group 8
+ // Yang Xu 500890631
+ // Ruoling Yu 500976267
+ // Xinyu Ma 500943173
+ // Vince De Vera 500550779
+ // Raynor Elgie 500964140
+ **/
+
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
-public class Client {
-	public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Syntax: QuoteClient <hostname> <port>");
-            return;
+
+public class Client
+{
+	public static void main(String[] args)
+    {
+        String hostname;
+        int port;
+
+        if (args.length < 2)
+        {
+            System.out.println("Usage: <hostname> <port>\nInput: ");
+            Scanner scanner = new Scanner(System.in);
+            hostname = scanner.next();
+            port = Integer.parseInt(scanner.next());
         }
- 
-        String hostname = args[0];
-        int port = Integer.parseInt(args[1]);
- 
-        try {
-			
+        else
+        {
+            hostname = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+
+        try
+        {
             InetAddress address = InetAddress.getByName(hostname);
             DatagramSocket socket = new DatagramSocket();
 			socket.connect(address, port);
 			
-            while (true) {
- 
+            while (true)
+            {
 				String packetString = "S000000000000000"; //S for SYN
 				String charsetName = "IBM01140";
 
@@ -36,14 +57,17 @@ public class Client {
                 Thread.sleep(1000);
             }
  
-        } catch (SocketTimeoutException ex) {
+        }
+        catch (SocketTimeoutException ex)
+        {
             System.out.println("Timeout error: " + ex.getMessage());
             ex.printStackTrace();
-        } catch (IOException ex) {
+        }
+        catch (IOException ex)
+        {
             System.out.println("Client error: " + ex.getMessage());
             ex.printStackTrace();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
         }
+        catch (InterruptedException ex) { ex.printStackTrace(); }
     }
 }
