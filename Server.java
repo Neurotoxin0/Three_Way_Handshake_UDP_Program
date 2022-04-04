e20aff41b59cc734d9bef408a4fc8be0b425896f
Don <Xusi101066@163.com>
2022-04-03 22:42:12 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -10,13 +10,13 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.Random;
 
 public class Server
 {
     private DatagramSocket socket;
     private List<String> listQuotes = new ArrayList<String>();
     private Random random;
+	byte[] buffer;
  
     public Server(int port) throws SocketException
 	{
@@ -28,20 +28,29 @@ public class Server
 	{
 		int port;
 
-		if (args.length < 1)
+		if (args.length != 1)
 		{
-            System.out.println("Usage: <port>\nInput: ");
+			System.out.println("--------------------------------------------------");
+			System.out.println("< Invalid Number of Argument, Reading From User >");
+			System.out.println("--------------------------------------------------");
+			System.out.print("Usage:\t<port>\n> ");
 			Scanner scanner = new Scanner(System.in);
+
 			port = Integer.parseInt(scanner.next());
         }
 		else { port = Integer.parseInt(args[0]);}
 
+		System.out.println("\nServing On Port: " + port);
+		System.out.println("--------------------------------------------------");
+
 		try
 		{
             Server server = new Server(port);
-			String[] detail = server.waitForSyn().replace("/","").split(":");
-            InetAddress client_ip = InetAddress.getByName(detail[0]);
-			int client_port = Integer.parseInt(detail[1]);
+
+			String[] client_detail = server.waitForSyn().replace("/","").split(":");
+            InetAddress client_ip = InetAddress.getByName(client_detail[0]);
+			int client_port = Integer.parseInt(client_detail[1]);
+
 			server.startConn(client_ip, client_port);
 			server.sendData(client_ip, client_port, "This is the data. Here is a second sentence that we'll send. Here is a third...", true);
 			server.sendFIN(client_ip, client_port);
@@ -53,61 +62,75 @@ public class Server
 	private String waitForSyn() throws IOException
 	{
 		DatagramSocket socket = this.socket;
-		byte[] buffer = new byte[16];
+		buffer = new byte[16];
+
 		while(true)
 		{
-			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-			try{
-				socket.receive(response);
-				
+			DatagramPacket msg_in = new DatagramPacket(buffer, buffer.length);
+
+			try
+			{
+				socket.receive(msg_in);
+				String client_ip = String.valueOf(msg_in.getAddress());
+				int client_port = msg_in.getPort();
+				System.out.println("Incoming Client: " + client_ip + ":" + client_port + "\n");
+
 				String res = new String(buffer, "IBM01140");
-				System.out.println("Recieved: " + res);
-				if(res.charAt(0) == 'S'){
-					System.out.println("(SYN MSG)");
-					return response.getAddress().toString()+":"+response.getPort();
+				System.out.println("Received: " + res);
+
+				if(res.charAt(0) == 'S')
+				{
+					System.out.println("< SYN Received >");
+					return client_ip + ":" + client_port;
 				}
-			}catch(SocketException e){
-				System.out.println("Timeout occured...");
 			}
+			catch(SocketException e)	{ System.out.println("SocketException: Timeout Occurred"); }
 		}
 	}
 	
 	private void startConn(InetAddress ip, int port) throws IOException, InterruptedException
 	{
 		DatagramSocket socket = this.socket;
-		System.out.println(ip);
-		System.out.println(port);
 		socket.connect(ip, port);
-		String packetString = "Z000000000000000"; // Z for synack
-		byte[] buffer = new byte[16];
+
+		System.out.println("...\nPreparing SYN-ACK\n...");
+		String packetString = "Z000000000000000"; // Z for SYN ACK
 		buffer = packetString.getBytes("IBM01140");
-		DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-		System.out.println("Sending: " + new String(buffer, "IBM01140"));
+		DatagramPacket msg_out = new DatagramPacket(buffer, buffer.length);
 		Thread.sleep(1000);
-        socket.send(request);
-		System.out.println("Sent SYNACK");
+        socket.send(msg_out);
+		System.out.println("< SYN-ACK Sent > - " + packetString);
+		System.out.println("...\nAwait REQ\n...");
+
 		while(true)
 		{
-			socket.connect(ip, port);
-			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-			try{
-				socket.receive(response);
+			DatagramPacket msg_in = new DatagramPacket(buffer, buffer.length);
+
+			try
+			{
+				socket.receive(msg_in);
 				String res = new String(buffer, "IBM01140");
-				System.out.println("Recieved packet: " + res);
-				if(res.charAt(0) == 'R')	{ return ; }	// Request recieved
-			}catch(SocketTimeoutException e){
-				System.out.println("Timeout occured...");
-				continue;
+				System.out.println("Received packet: " + res);
+
+				if(res.charAt(0) == 'R')	// Request received
+				{
+					System.out.println("< REQ Received >");
+					System.out.println("--------------------------------------------------");
+					return ;
+				}
 			}
+			catch(SocketTimeoutException e) { System.out.println("SocketTimeoutException: Timeout Occurred"); }
 		}
 	}
 	
-	private void sendData(InetAddress ip, int port, String data, boolean loss) throws IOException{
+	private void sendData(InetAddress ip, int port, String data, boolean loss) throws IOException
+	{
 		DatagramSocket socket = this.socket;
 		socket.connect(ip, port);
-		
-		Random rand = new Random();
-		
+		Random random =this.random;
+
+		System.out.println("Splitting Messages into Chunks:\n");
+
 		int ceil = ((int) Math.ceil((double)data.length() / 14.0)) * 14;
 		data = String.format("%-" + ceil + "s", data);
 		
@@ -115,45 +138,51 @@ public class Server
 		ArrayList<byte[]> chunks = new ArrayList<byte[]>();
 		
 		int i = 0;
-		while((i+1) * 14 <= allBytes.length){
-			System.out.println((i * 14) + " to " + ((i + 1) * 14));
+
+		while((i+1) * 14 <= allBytes.length)
+		{
+			System.out.println("Chunk " + i + ": " + (i * 14) + " to " + ((i + 1) * 14));
 			byte[] chunk = Arrays.copyOfRange(allBytes, (i * 14), ((i + 1) * 14));
 			chunks.add(chunk);
 			//System.out.println(new String(chunk, "IBM01140"));
 			i += 1;
 		}
 		
-		byte[] buffer = new byte[16];
+		buffer = new byte[16];
 		byte[] recvBuffer = new byte[16];
-		for(i = 0; i < chunks.size(); i++){
-			while(true){ //TODO: HANDLE TIMEOUT
+		System.out.println("\nSending Messages:");
+
+		for(i = 0; i < chunks.size(); i++)
+		{
+			while(true)	// TODO: HANDLE TIMEOUT
+			{
 				buffer = ("D" + (i % 2) + "00000000000000").getBytes("IBM01140");
 				System.arraycopy(chunks.get(i), 0, buffer, 2, 14);
 
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-				System.out.println("Sending: " + new String(buffer, "IBM01140"));
-				if(!loss || rand.nextInt(5) < 4){
-					socket.send(request);
-				}else{
-					System.out.println("(Simulated loss event)");
-				}
+				System.out.println("\nSending: " + new String(buffer, "IBM01140"));
+
+				if(!loss || random.nextInt(5) < 4) { socket.send(request); }
+				else { System.out.println("< Simulating Loss Event >"); }
+
 				DatagramPacket response = new DatagramPacket(recvBuffer, recvBuffer.length);
 				socket.setSoTimeout(400);
-				try{
+
+				try
+				{
 					socket.receive(response);
 					String res = new String(recvBuffer, "IBM01140");
-					System.out.println("Recieved packet: " + res);
-					if(res.charAt(0) == 'A' && Character.getNumericValue(res.charAt(1)) == (i % 2))	{ break; }	// Correct ack
-				}catch(SocketTimeoutException e){
-					System.out.println("Timeout occured...");
-					continue;
+
+					if(res.charAt(0) == 'A' && Character.getNumericValue(res.charAt(1)) == (i % 2))	// Correct ack
+					{
+						System.out.println("< ACK Received > - " + res);
+						break;
+					}
+					else { System.out.println("< Invalid ACK Received! > - " + res); }
 				}
-				
+				catch(SocketTimeoutException e) { System.out.println("< ACK Loss > - Re-transmitting Current Chunk"); }
 			}
-			
 		}
-		
-		return;
 	}
 	
 	private void sendFIN(InetAddress ip, int port) throws IOException	//CHECK IF WE SHOULD ADD SEQ # TODO: HANDLE TIMEOUT
@@ -161,23 +190,28 @@ public class Server
 		DatagramSocket socket = this.socket;
 		socket.connect(ip, port);
 		String packetString = "F000000000000000"; //F for FIN
-		byte[] buffer = new byte[16];
 		buffer = packetString.getBytes("IBM01140");
-		DatagramPacket request = new DatagramPacket(buffer, 2);
-		socket.send(request);
+		DatagramPacket msg_out = new DatagramPacket(buffer, 2);
+		socket.send(msg_out);
+		System.out.println("\n< FIN Sent > - " + packetString);
+
 		
 		while(true)
 		{
 			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
 			socket.setSoTimeout(400);
-			try{
+			try
+			{
 				socket.receive(response);
 				String res = new String(buffer, "IBM01140");
-				if(res.charAt(0) == 'A')	{ return; }	//Ack recieved
-			}catch(SocketTimeoutException e){
-				System.out.println("Timeout occured...");
-				continue;
+				if(res.charAt(0) == 'A')	//Ack received
+					{
+						System.out.println("< FIN-ACK Received > - " + res);
+						System.out.println("--------------------------------------------------");
+						return ;
+					}
 			}
+			catch(SocketTimeoutException e) { System.out.println("< FIN-ACK Loss > - Re-transmitting FIN"); }
 		}
 	}
 }
